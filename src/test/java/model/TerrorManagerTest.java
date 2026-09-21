b20f96d8d0e283aeae8246d1e19e0d9119c6dd9c
pietro-zordan <zordanpietro05@gmail.com>
2026-09-21 11:09:54 +0200

    Test terror spawn after happiness collapse

--- src/test/java/model/TerrorManagerTest.java
+++ src/test/java/model/TerrorManagerTest.java
@@ -50,6 +50,42 @@ class TerrorManagerTest
     }
 
     @Test
+    void terroristGroupAppearsAfterHappinessDropsBy1000InOneTick()
+    {
+        Grid grid = new Grid();
+        grid.restoreConstruction(new Road(), 10, 10);
+
+        City city = new City(grid, new StandardPolicy());
+        TerrorManager terrorManager = new TerrorManager(city, grid);
+
+        city.decreaseGlobalHappiness(1000);
+
+        assertTrue(terrorManager.updateOfOneTick());
+        assertEquals(1, terrorManager.getNumOfTG());
+    }
+
+    @Test
+    void terroristGroupAppearsAfterHappinessDropsBy1000WithinThreeTicks()
+    {
+        Grid grid = new Grid();
+        grid.restoreConstruction(new Road(), 10, 10);
+
+        City city = new City(grid, new StandardPolicy());
+        TerrorManager terrorManager = new TerrorManager(city, grid);
+
+        city.decreaseGlobalHappiness(300);
+        terrorManager.updateOfOneTick();
+
+        city.decreaseGlobalHappiness(300);
+        terrorManager.updateOfOneTick();
+
+        city.decreaseGlobalHappiness(400);
+
+        assertTrue(terrorManager.updateOfOneTick());
+        assertEquals(1, terrorManager.getNumOfTG());
+    }
+
+    @Test
     void terrorSpawnWorksWithNegativeBudget()
     {
         Grid grid = new Grid();

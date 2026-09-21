448382ed97b27ba8002cb2a8ec45f65bf1122c1b
pietro-zordan <zordanpietro05@gmail.com>
2026-09-21 09:34:02 +0200

    Decouple construction removal from PowerPlant

--- src/main/java/model/Construction.java
+++ src/main/java/model/Construction.java
@@ -118,7 +118,7 @@ public abstract class Construction {
     {
         if (isPowerPlantConnected())
         {
-            connectedPowerPlant.removeConstruction(this);
+            disconnectFromPowerPlant();
         }
     }
 

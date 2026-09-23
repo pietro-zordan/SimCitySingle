0e47025af8da3886c5fc5831ae113cd844f5b740
pietro-zordan <zordanpietro05@gmail.com>
2026-09-23 08:32:41 +0200

    balancement

--- src/main/java/Events/MissileAttack.java
+++ src/main/java/Events/MissileAttack.java
@@ -19,7 +19,7 @@ public class MissileAttack extends Event
         super(TICK_DURATION, city, 200);
 
         this.grid=grid;
-        launchNumber= 10;
+        launchNumber= 8;
     }
 
     @Override

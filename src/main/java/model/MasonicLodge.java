0e3fafe5321a9fd674b049e43c4a6931f4f46ab5
pietro-zordan <zordanpietro05@gmail.com>
2026-09-24 12:05:51 +0200

    gg

---
+++ src/main/java/model/MasonicLodge.java
@@ -0,0 +1,18 @@
+package model;
+
+public class MasonicLodge extends Construction{
+
+    public MasonicLodge()
+    {
+        super(0, 20, 0);
+    }
+
+    @Override
+    public ConstructionType getType() {
+        return null;
+    }
+
+    
+
+
+}
